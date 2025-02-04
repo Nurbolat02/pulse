10c1b5569b339a09bff69cbf22ca311f1e3a10bf
Nurbolat <sdfgasw@mail.ru>
2025-02-04 12:36:14 +0100

    tinyslider

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,31 @@
-$(document).ready(function () {
-    $('.carousel__inner').slick({
-        speed: 1200,
-        // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="./icons/left.svg" alt="img"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="./icons/right.svg" alt="img"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false,
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    nav: false,
+    controls: false,
 
-                }
-            }]
-    });
 });
+document.querySelector('.prev').onclick = function () {
+    slider.goTo('prev');
+};
+document.querySelector('.next').onclick = function () {
+    slider.goTo('next');
+};
+// $(document).ready(function () {
+//     $('.carousel__inner').slick({
+//         speed: 1200,
+//         // adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="./icons/left.svg" alt="img"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="./icons/right.svg" alt="img"></button>',
+//         responsive: [
+//             {
+//                 breakpoint: 992,
+//                 settings: {
+//                     dots: true,
+//                     arrows: false,
+
+//                 }
+//             }]
+//     });
+// });
